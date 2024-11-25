ebb4771a9fd351e34f4c744986fd975dc178d76b
WM <wm@WMs-MacBook-Pro.local>
2024-11-24 19:27:27 -0500

    refactor(configuration): reorganize system configuration and fix attribute structure
    - Combined all system attributes into a single system attribute set
    - Moved programs.nix-index and services.nix-daemon to top level
    - Reorganized environment.systemPackages
    - Improved dock configuration structure
    - Fixed activation script indentation

--- nixos/hosts/wm-macbook-pro/configuration.nix
+++ nixos/hosts/wm-macbook-pro/configuration.nix
@@ -11,11 +11,67 @@
     ../../../modules/nix-darwin
   ];
 
-  environment.variables = {
-    EDITOR = "nvim";
-    VISUAL = "nvim";
-    # Ensure proper clipboard support
-    TERM = "xterm-256color";
+  environment = {
+    variables = {
+      EDITOR = "nvim";
+      VISUAL = "nvim";
+      GIT_EDITOR = "nvim";
+      # Ensure proper clipboard support
+      TERM = "xterm-256color";
+    };
+    systemPackages = with pkgs; [
+      # Development tools
+      git
+      neovim
+      tmux
+      zsh
+      wget
+      curl
+      htop
+      tree
+      ripgrep
+      fd
+      jq
+      yq-go
+      fzf
+      bat
+      eza
+      nix-direnv
+      direnv
+      gnumake
+      cmake
+      ninja
+      gcc
+      go
+      rustup
+      nodejs
+      python3
+      python3Packages.pip
+      python3Packages.pipx
+
+      # Build tools
+      pkg-config
+      autoconf
+      automake
+      libtool
+
+      # Debugging and analysis
+      gdb
+      lldb
+
+      # Additional CLI tools
+      git-crypt
+      pre-commit
+      shellcheck
+      nixpkgs-fmt
+      alejandra
+      deadnix
+      statix
+      mkalias
+      pam-reattach
+      obsidian
+    ];
+    shells = [pkgs.zsh];
   };
 
   # Nix configuration
@@ -34,18 +90,13 @@
     };
   };
 
-  # System Packages
-  environment.systemPackages = with pkgs; [
-    mkalias
-    obsidian
-    pam-reattach
-  ];
-
   # Enable touch ID for sudo authentication
   security.pam.enableSudoTouchIdAuth = true;
 
-  # Add pam-reattach for TouchID support in tmux
-  system.activationScripts.postActivation.text = ''
+  # System configuration
+  system = {
+    # Add pam-reattach to enable TouchID for tmux and set up applications
+    activationScripts.postActivation.text = ''
         # Add pam_reattach to enable TouchID for tmux
         sudo mkdir -p /usr/local/lib/pam
         sudo cp ${pkgs.pam-reattach}/lib/pam/pam_reattach.so /usr/local/lib/pam/
@@ -53,87 +104,46 @@
         # Add pam_reattach to sudo config if not already present
         if ! grep -q "pam_reattach.so" /etc/pam.d/sudo; then
           sudo sed -i "" '2i\
-    auth       optional     pam_reattach.so
-    ' /etc/pam.d/sudo
+      auth       optional     pam_reattach.so
+      ' /etc/pam.d/sudo
         fi
-  '';
 
-  # Enable nix daemon service.
+        # Set up applications
+        echo "setting up /Applications..." >&2
+        rm -rf /Applications/Nix\ Apps
+        mkdir -p /Applications/Nix\ Apps
+        find ${pkgs.buildEnv {
+        name = "system-applications";
+        paths = config.environment.systemPackages;
+        pathsToLink = "/Applications";
+      }}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+          while read -r src; do
+            app_name=$(basename "$src")
+            echo "copying $src" >&2
+            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+          done
+    '';
+
+    # System defaults
+    defaults.dock = {
+      autohide = true;
+      mru-spaces = false;
+      minimize-to-application = true;
+      show-recents = false;
+      static-only = true;
+      showhidden = true;
+    };
+
+    configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
+    stateVersion = 4;
+  };
+
+  # Enable nix daemon service
   services.nix-daemon.enable = true;
 
   # Enable nix-index service to update nix packages
   programs.nix-index.enable = true;
 
-  # Set Git commit hash for darwin-version.
-  system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
-
-  # Used for backwards compatibility, please read the changelog before changing.
-  # $ darwin-rebuild changelog
-  system.stateVersion = 4;
-
-  # The platform the configuration will be used on.
-  nixpkgs.hostPlatform = "x86_64-darwin";
-  nixpkgs.config.allowUnfree = true;
-
-  # Declare the user that will be running `nix-darwin`.
-  users.users.wm = {
-    name = "wm";
-    home = "/Users/wm";
-    shell = pkgs.zsh;
-  };
-
-  fonts = {
-    packages = with pkgs; [
-      (nerdfonts.override {
-        fonts = [
-          "JetBrainsMono"
-          "FiraCode"
-          "IBMPlexMono"
-        ];
-      })
-      noto-fonts
-      noto-fonts-cjk-sans
-      noto-fonts-emoji
-    ];
-  };
-
-  system.activationScripts.applications.text = let
-    env = pkgs.buildEnv {
-      name = "system-applications";
-      paths = config.environment.systemPackages;
-      pathsToLink = "/Applications";
-    };
-  in
-    pkgs.lib.mkForce ''
-      # Set up applications.
-      echo "setting up /Applications..." >&2
-      rm -rf /Applications/Nix\ Apps
-      mkdir -p /Applications/Nix\ Apps
-      find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-      while read -r src; do
-        app_name=$(basename "$src")
-        echo "copying $src" >&2
-        ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-      done
-    '';
-
-  system.defaults = {
-    dock.autohide = true;
-    dock.mru-spaces = false;
-    dock.orientation = "bottom";
-    dock.persistent-apps = [
-      "${pkgs.alacritty}/Applications/Alacritty.app"
-      "/Applications/Firefox.app"
-      "${pkgs.obsidian}/Applications/Obsidian.app"
-      "/System/Applications/Mail.app"
-      "/System/Applications/Calendar.app"
-    ];
-    finder.AppleShowAllExtensions = true;
-    finder.FXPreferredViewStyle = "clmv";
-    loginwindow.GuestEnabled = false;
-    screencapture.location = "~/Pictures/screenshots";
-    screensaver.askForPasswordDelay = 10;
-  };
   homebrew = {
     enable = true;
     brews = [
@@ -150,15 +160,40 @@
     ];
     masApps = {
     };
-    onActivation.cleanup = "zap";
-    onActivation.autoUpdate = true;
-    onActivation.upgrade = true;
+    onActivation = {
+      cleanup = "zap";
+      autoUpdate = true;
+      upgrade = true;
+    };
   };
   nix-homebrew.user = "wm";
 
-  # Set zsh as default shell globally
-  environment.shells = [pkgs.zsh];
-
   # Enable zsh system-wide
   programs.zsh.enable = true;
+
+  fonts = {
+    packages = with pkgs; [
+      (nerdfonts.override {
+        fonts = [
+          "JetBrainsMono"
+          "FiraCode"
+          "IBMPlexMono"
+        ];
+      })
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
+    ];
+  };
+
+  # Declare the user that will be running `nix-darwin`.
+  users.users.wm = {
+    name = "wm";
+    home = "/Users/wm";
+    shell = pkgs.zsh;
+  };
+
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "x86_64-darwin";
+  nixpkgs.config.allowUnfree = true;
 }
