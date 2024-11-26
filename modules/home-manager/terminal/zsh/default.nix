56c6e8845940cb99b297aa200dc6cf94cd7fe5ee
hackiri <128340174+Hackiri@users.noreply.github.com>
2024-11-26 08:39:10 -0500

    feat(zsh): enhance oh-my-zsh configuration
    - Remove starship configuration
    - Set agnoster as the default theme
    - Add more useful oh-my-zsh plugins
    - Increase history size to 50000
    - Enable history substring search
    - Improve shell environment variables

--- modules/home-manager/terminal/zsh/default.nix
+++ modules/home-manager/terminal/zsh/default.nix
@@ -7,90 +7,153 @@
   zshDotDir = ".config/zsh";
   shellAliases = import ./aliases.nix;
 in {
-  home.sessionVariables = {
-    KREW_ROOT = "${config.home.homeDirectory}/.krew";
-    PATH = "${config.home.homeDirectory}/.krew/bin:${config.home.homeDirectory}/bin:$PATH";
-    GIT_USER_NAME = "hackiri";
-    GIT_USER_EMAIL = "128340174+Hackiri@users.noreply.github.com";
+  home = {
+    sessionVariables = {
+      KREW_ROOT = "${config.home.homeDirectory}/.krew";
+      PATH = "${config.home.homeDirectory}/.krew/bin:${config.home.homeDirectory}/bin:$PATH";
+      GIT_USER_NAME = "hackiri";
+      GIT_USER_EMAIL = "128340174+Hackiri@users.noreply.github.com";
+      EDITOR = "vim";
+      VISUAL = "vim";
+      LANG = "en_US.UTF-8";
+      LC_ALL = "en_US.UTF-8";
+      TERM = "xterm-256color";
+      # oh-my-zsh configuration
+      ZSH = "${pkgs.oh-my-zsh}/share/oh-my-zsh";
+      ZSH_CUSTOM = "${config.home.homeDirectory}/.oh-my-zsh/custom";
+      ZSH_CACHE_DIR = "${config.home.homeDirectory}/.cache/oh-my-zsh";
+    };
+
+    packages = with pkgs; [
+      direnv
+      fzf
+      zoxide
+      bat
+      jq
+      oh-my-zsh
+      python3Packages.pygments  # Required for syntax highlighting
+    ];
   };
 
   programs = {
     zsh = {
       enable = true;
       inherit shellAliases;
-      autosuggestion.enable = true;
+      enableCompletion = true;
+
+      # Enable native plugins
       syntaxHighlighting.enable = true;
-      history.size = 10000;
-      history.path = "${config.home.homeDirectory}/.zsh_history";
+      autosuggestion.enable = true;
+      historySubstringSearch.enable = true;
+
+      history = {
+        size = lib.mkForce 50000;
+        save = lib.mkForce 50000;
+        path = "${config.home.homeDirectory}/.zsh_history";
+        ignoreDups = true;
+        share = true;
+        extended = true;
+      };
       dotDir = zshDotDir;
+
       oh-my-zsh = {
         enable = true;
-        theme = ""; # We're using starship instead
+        package = pkgs.oh-my-zsh;
+        theme = "agnoster";
         plugins = [
-          "git" # Git is usually available
-          "sudo" # No dependencies needed
-          "history" # No dependencies needed
-          "direnv" # Will be installed via home.packages
-          "copypath" # No dependencies needed
-          "copyfile" # No dependencies needed
-          "extract" # No dependencies needed
-          "z" # No dependencies needed
-          "macos" # macOS specific, no extra deps
+          "git"
+          "sudo"
+          "history"
+          "direnv"
+          "fzf"
+          "zoxide"
+          "z"
+          "extract"
+          "colored-man-pages"
+          "command-not-found"
+          "aliases"
+          "alias-finder"
+          "common-aliases"
+          "copypath"
+          "copyfile"
+          "copybuffer"
+          "dirhistory"
+          "docker"
+          "docker-compose"
+          "kubectl"
+          "npm"
+          "pip"
+          "python"
+          "rust"
+          "golang"
+          "macos"
+          "vscode"
+          "web-search"
+          "jsontools"
         ];
       };
-      plugins = [
-        {
-          name = "zsh-autosuggestions";
-          src = pkgs.fetchFromGitHub {
-            owner = "zsh-users";
-            repo = "zsh-autosuggestions";
-            rev = "v0.7.0";
-            sha256 = "sha256-KLUYpUu4DHRumQZ3w59m9aTW6TBKMCXl2UcKi4uMd7w=";
-          };
-        }
-        {
-          name = "zsh-syntax-highlighting";
-          src = pkgs.fetchFromGitHub {
-            owner = "zsh-users";
-            repo = "zsh-syntax-highlighting";
-            rev = "0.7.1";
-            sha256 = "sha256-gOG0NLlaJfotJfs+SUhGgLTNOnGLjoqnUp54V9aFJg8=";
-          };
-        }
-      ];
 
       initExtra = ''
+        # Ensure cache directory exists
+        if [[ ! -d "$ZSH_CACHE_DIR" ]]; then
+          mkdir -p "$ZSH_CACHE_DIR"
+        fi
+
+        # Source oh-my-zsh
+        if [ -f "$ZSH/oh-my-zsh.sh" ]; then
+          source "$ZSH/oh-my-zsh.sh"
+        else
+          echo "Warning: oh-my-zsh.sh not found at $ZSH/oh-my-zsh.sh"
+        fi
+
         # Basic configurations
         zstyle ':completion:*' matcher-list "" 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
         WORDCHARS='*?_-.[]~=&;!#$%^(){}<>'
-
-        # Initialize Starship
-        eval "$(starship init zsh)"
 
         # Fix for zle warnings
         zmodload zsh/zle
         zmodload zsh/zpty
         zmodload zsh/complete
 
-        setopt interactive_comments bashautolist nobeep nomenucomplete \
-                noautolist extended_glob
+        # Advanced ZSH options
+        setopt AUTO_CD
+        setopt AUTO_PUSHD
+        setopt PUSHD_IGNORE_DUPS
+        setopt PUSHD_MINUS
+        setopt EXTENDED_HISTORY
+        setopt HIST_EXPIRE_DUPS_FIRST
+        setopt HIST_IGNORE_DUPS
+        setopt HIST_IGNORE_SPACE
+        setopt HIST_VERIFY
+        setopt SHARE_HISTORY
+        setopt INTERACTIVE_COMMENTS
+        setopt COMPLETE_IN_WORD
+        setopt ALWAYS_TO_END
+        setopt PATH_DIRS
+        setopt AUTO_MENU
+        setopt AUTO_LIST
+        setopt AUTO_PARAM_SLASH
+        setopt NO_BEEP
+
+        # Better directory navigation
+        alias ..='cd ..'
+        alias ...='cd ../..'
+        alias ....='cd ../../..'
+        alias .....='cd ../../../..'
+
+        # Initialize zoxide
+        eval "$(zoxide init zsh)"
+
+        # FZF configuration
+        export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+        export FZF_DEFAULT_OPTS="--height 50% -1 --layout=reverse --multi"
+      '';
+
+      profileExtra = ''
+        if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then
+          . $HOME/.nix-profile/etc/profile.d/nix.sh
+        fi
       '';
     };
-
-    starship = {
-      enable = lib.mkForce true;
-      enableZshIntegration = lib.mkForce true;
-      settings = lib.mkForce {};
-    };
-  };
-
-  # Install necessary packages for plugins
-  home.packages = with pkgs; [
-    direnv
-    fzf
-  ];
-
-  xdg.configFile."starship.toml" = {
-    source = ./themes/jetpack.toml;
   };
 }
