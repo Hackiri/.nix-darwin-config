0aef27f42dc9bb96fe518e0552fb63c33f58a7aa
hackiri <128340174+Hackiri@users.noreply.github.com>
2025-04-20 11:30:52 -0400

    Fix fonts module: use home.packages for Home Manager

--- modules/home-manager/fonts/default.nix
+++ modules/home-manager/fonts/default.nix
@@ -1,15 +1,11 @@
 # Home Manager module for font configuration
 { pkgs, ... }: {
-  fonts = {
-    fontconfig.enable = true;
-    packages = with pkgs; [
-      nerd-fonts.jetbrains-mono
-      nerd-fonts.fira-code
-      nerd-fonts.blex-mono
-      noto-fonts
-      noto-fonts-cjk-sans
-      noto-fonts-emoji
-    ];
-  };
+  home.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.fira-code
+    nerd-fonts.blex-mono
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-emoji
+  ];
 }
-
