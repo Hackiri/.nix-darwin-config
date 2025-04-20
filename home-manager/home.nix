0d59950eb7612a7bf8aa891c628f9a0253e9426a
hackiri <128340174+Hackiri@users.noreply.github.com>
2025-04-20 11:32:19 -0400

    Remove config.options.cliPackages from home.packages to fix build error

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -12,10 +12,8 @@ in {
     ../modules/home-manager
   ];
 
-  fonts.fontconfig.enable = true;
 
   # Enable devshell
-  programs.devshell.enable = true;
 
   home = {
     username = "wm";
@@ -24,45 +22,13 @@ in {
 
     # Packages to be installed for the user
     packages = with pkgs; [
-      # Development utilities
-      pre-commit
-      shellcheck
-      alejandra
-      deadnix
-      statix
-      stylua
-      devenv
       customPkgs.dev-tools # Custom development helper scripts
-
-      # Debugging and analysis
-      gdb
-      lldb
-      viu
-
-      # Network utilities
-      bind # Provides dig command
-
-      # Kubernetes and infrastructure tools
-      omnictl
-      talosctl
-      terraform
-      cilium-cli
-      kustomize
-      k9s
-      kubectl
-      kubecolor
-      krew
-      kubernetes-helm
-      kubernetes-helmPlugins.helm-diff
-      kubernetes-helmPlugins.helm-secrets
     ];
 
-    sessionPath = [
-      "/run/current-system/sw/bin"
-      "$HOME/.nix-profile/bin"
-    ];
+
   };
 
   programs = {
+    # Git configuration can be added here
   };
 }
