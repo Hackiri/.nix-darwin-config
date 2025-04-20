027f422925d097ff9faaa5760d37f380fe7b2ef2
hackiri <128340174+Hackiri@users.noreply.github.com>
2025-04-20 11:25:22 -0400

    Track zellij home-manager module for flake build

---
+++ modules/home-manager/cli/zellij/default.nix
@@ -0,0 +1,16 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  imports = [];
+
+  programs.zellij = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  # Link zellij configuration
+  # xdg.configFile."zellij/config.kdl".source = ./config.kdl;
+}
