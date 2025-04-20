720c90149afe04368d56b0dedf12936762ff0329
hackiri <128340174+Hackiri@users.noreply.github.com>
2025-04-20 11:36:08 -0400

    Remove theme argument from yazi module and use static colors to fix build error

--- modules/home-manager/yazi/default.nix
+++ modules/home-manager/yazi/default.nix
@@ -1,4 +1,8 @@
-{ config, pkgs, theme, ... }: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   programs.yazi = {
     enable = true;
     enableZshIntegration = true;
@@ -9,45 +13,45 @@
       };
       theme = {
         manager = {
-          tab_normal.bg = {hex = theme.colors.bg;};
-          tab_normal.fg = {hex = theme.colors.comment;};
-          tab_select.bg = {hex = theme.colors.bg;};
-          tab_select.fg = {hex = theme.colors.blue;};
+          tab_normal.bg = {hex = "#282a36";};
+          tab_normal.fg = {hex = "#6272a4";};
+          tab_select.bg = {hex = "#282a36";};
+          tab_select.fg = {hex = "#8be9fd";};
           border_symbol = "│";
-          border_style.fg = {hex = theme.colors.blue0;};
-          selection.bg = {hex = theme.colors.blue0;};
-          selection.fg = {hex = theme.colors.fg;};
-          status_normal.bg = {hex = theme.colors.bg;};
-          status_normal.fg = {hex = theme.colors.fg_dark;};
-          status_select.bg = {hex = theme.colors.blue0;};
-          status_select.fg = {hex = theme.colors.fg;};
-          folder.fg = {hex = theme.colors.blue;};
-          link.fg = {hex = theme.colors.green1;};
-          exec.fg = {hex = theme.colors.green;};
+          border_style.fg = {hex = "#8be9fd";};
+          selection.bg = {hex = "#8be9fd";};
+          selection.fg = {hex = "#f8f8f2";};
+          status_normal.bg = {hex = "#282a36";};
+          status_normal.fg = {hex = "#44475a";};
+          status_select.bg = {hex = "#8be9fd";};
+          status_select.fg = {hex = "#f8f8f2";};
+          folder.fg = {hex = "#8be9fd";};
+          link.fg = {hex = "#50fa7b";};
+          exec.fg = {hex = "#50fa7b";};
         };
         preview = {
-          hovered.bg = {hex = theme.colors.bg_highlight;};
-          hovered.fg = {hex = theme.colors.fg;};
+          hovered.bg = {hex = "#44475a";};
+          hovered.fg = {hex = "#f8f8f2";};
         };
         input = {
-          border.fg = {hex = theme.colors.blue;};
-          title.fg = {hex = theme.colors.fg_dark;};
-          value.fg = {hex = theme.colors.fg;};
-          selected.bg = {hex = theme.colors.blue0;};
+          border.fg = {hex = "#8be9fd";};
+          title.fg = {hex = "#44475a";};
+          value.fg = {hex = "#f8f8f2";};
+          selected.bg = {hex = "#8be9fd";};
         };
         completion = {
-          border.fg = {hex = theme.colors.blue;};
-          selected.bg = {hex = theme.colors.blue0;};
+          border.fg = {hex = "#8be9fd";};
+          selected.bg = {hex = "#8be9fd";};
         };
         tasks = {
-          border.fg = {hex = theme.colors.blue;};
-          selected.bg = {hex = theme.colors.blue0;};
+          border.fg = {hex = "#8be9fd";};
+          selected.bg = {hex = "#8be9fd";};
         };
         which = {
-          mask.bg = {hex = theme.colors.bg;};
-          desc.fg = {hex = theme.colors.comment;};
-          selected.bg = {hex = theme.colors.blue0;};
-          selected.fg = {hex = theme.colors.fg;};
+          mask.bg = {hex = "#282a36";};
+          desc.fg = {hex = "#6272a4";};
+          selected.bg = {hex = "#8be9fd";};
+          selected.fg = {hex = "#f8f8f2";};
         };
       };
     };
